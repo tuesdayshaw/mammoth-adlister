33196d896dd770f9eece9cf83dbe6048eef18b2e
Tuesday Shaw <tuesday.shaw@gmail.com>
2017-06-07 09:09:03 -0500

    Create a directory inside webapps named partials.
    Create separate files for the common elements of your website that appear on every
    page (for example, a navbar or your css style sheets) inside the partials directory.
    Include these files on your other pages, and any new files you create.

--- src/main/java/CounterServlet.java
+++ src/main/java/CounterServlet.java
@@ -1,15 +1,32 @@
+/**
+ * Created by canidmars on 6/6/17.
+ */
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "CounterServlet", urlPatterns = "/count")
 public class CounterServlet extends HttpServlet {
-    private int counter = 0;
+
+    private int count = 0;
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        counter += 1;
-        response.getWriter().println("<h1>The count is " + counter + ".</h1>");
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        String c2 = request.getParameter("c2");
+
+        if (c2 != null){
+            count = 0;
+        }
+
+        count++;
+
+        out.println("<h1>Page Count: "+ count +" !</h1>");
+
+
     }
 }
---
+++ src/main/webapp/partials/head.jsp
@@ -0,0 +1,13 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: canidmars
+  Date: 6/7/17
+  Time: 9:07 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
+<head>
+    <title>Index</title>
+</head>
+
