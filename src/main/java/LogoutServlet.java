448395423837c9ef1c72aeb11df9bef5bc4d9c42
Tuesday Shaw <tuesday.shaw@gmail.com>
2017-06-08 16:13:32 -0500

    Added invalidate session to double check all session/cookies were cleared

--- src/main/java/LogoutServlet.java
+++ src/main/java/LogoutServlet.java
@@ -18,6 +18,7 @@ public class LogoutServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getSession().removeAttribute("user");
+        request.getSession().invalidate();
         response.sendRedirect("/login");
     }
 }
