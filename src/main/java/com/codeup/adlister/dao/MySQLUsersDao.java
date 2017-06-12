06112e1d8d98af856a05fb59ecf143b3058cf94b
Tuesday Shaw <tuesday.shaw@gmail.com>
2017-06-12 15:10:30 -0500

    commiting to switch to another branch

--- src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -1,11 +1,43 @@
 package com.codeup.adlister.dao;
-
+import com.codeup.adlister.*;
+import com.codeup.adlister.models.Ad;
 import com.codeup.adlister.models.User;
+import com.mysql.cj.jdbc.Driver;
+
+import javax.jws.soap.SOAPBinding;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by canidmars on 6/12/17.
  */
 public class MySQLUsersDao implements Users {
+    private Connection connection;
+
+    public MySQLUsersDao(Config config) throws SQLException {
+        DriverManager.registerDriver(new Driver());
+
+        connection = DriverManager.getConnection(
+                config.getUrl(),
+                config.getUsername(),
+                config.getPassword()
+        );
+    }
+
+    public List<User> all() throws SQLException {
+
+        String query = "SELECT * FROM users";
+        try {
+            PreparedStatement stmt = connection.prepareStatement(query);
+            ResultSet rs = stmt.executeQuery();
+            return createUsersFromResults(rs);
+        } catch (SQLException e) {
+            throw new RuntimeException("Error retrieving all ads.", e);
+        }
+
+    }
+
     @Override
     public User findByUsername(String username) {
         return null;
@@ -13,6 +45,40 @@ public class MySQLUsersDao implements Users {
 
     @Override
     public Long insert(User user) {
-        return null;
+        try {
+            PreparedStatement stmt = connection.prepareStatement(createInsertQuery(user), Statement.RETURN_GENERATED_KEYS);
+            stmt.setString(1, user.getUsername());
+            stmt.setString(2, user.getEmail());
+            stmt.setString(3, user.getPassword());
+            stmt.executeUpdate();
+            ResultSet rs = stmt.getGeneratedKeys();
+            rs.next();
+            return rs.getLong(1);
+        } catch (SQLException e) {
+            throw new RuntimeException("Error creating a new user.", e);
+        }
+    }
+
+    private String createInsertQuery(User user) {
+        return "INSERT INTO users(username, email, password) VALUES (?, ?, ?)";
+    }
+
+
+    private User extractUser(ResultSet rs) throws SQLException {
+        return new User(
+                rs.getLong("id"),
+                rs.getString("username"),
+                rs.getString("email"),
+                rs.getString("password")
+        );
+    }
+
+
+    private List<User> createUsersFromResults(ResultSet rs) throws SQLException {
+        List<User> users = new ArrayList<>();
+        while (rs.next()) {
+            users.add(extractUser(rs));
+        }
+        return users;
     }
 }
