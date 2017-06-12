b3660ce0af8119c452320bd5a49dcf64872cd8d8
Tuesday Shaw <tuesday.shaw@gmail.com>
2017-06-12 14:36:02 -0500

    commiting to switch to another branch

---
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -0,0 +1,18 @@
+package com.codeup.adlister.dao;
+
+import com.codeup.adlister.models.User;
+
+/**
+ * Created by canidmars on 6/12/17.
+ */
+public class MySQLUsersDao implements Users {
+    @Override
+    public User findByUsername(String username) {
+        return null;
+    }
+
+    @Override
+    public Long insert(User user) {
+        return null;
+    }
+}
